fb1d417085909b92c0d8b236e359dbfc403a00c0
Alexander <qinglinhe44@gmail.com>
2024-09-09 11:37:45 +0800

    Modify constructor in User

--- src/main/java/bms/domain/User.java
+++ src/main/java/bms/domain/User.java
@@ -12,20 +12,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class User {
-
     private int id;
     private String username;
     private String password;
     private String imageUrl;
     private String email;
     private LocalDateTime createdTime;
-
-    public User(String username,  String email, String password, String imageUrl) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.imageUrl = imageUrl;
-        this.email = email;
-        this.createdTime = createdTime;
-    }
 }
